7672ed1d37e016351d907cb1aa98d1de58b91fdd
Bayram FRADJ <bayram.fraj@esprit.tn>
2021-03-13 02:40:04 +0100

    integrate template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ServiceComponent } from './Components/service/service.component';
+import { BlogComponent } from './Components/blog/blog.component';
+import { MissionComponent } from './Components/mission/mission.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ServiceComponent,
+    BlogComponent,
+    MissionComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
